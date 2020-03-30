dff328ca356e48902f14a8352a44a3ccb7831a57
Pablo Deyvid <pablodeyvid11@gmail.com>
2020-03-29 22:47:08 -0300

    Inserção de usuário com POST

--- src/main/java/com/pablodeyvid/springbootmongodb/services/UserService.java
+++ src/main/java/com/pablodeyvid/springbootmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pablodeyvid.springbootmongodb.domain.User;
+import com.pablodeyvid.springbootmongodb.dto.UserDTO;
 import com.pablodeyvid.springbootmongodb.repository.UserRepository;
 import com.pablodeyvid.springbootmongodb.services.exception.ObjectNotFoundException;
 
@@ -16,7 +17,7 @@ public class UserService {
 	@Autowired
 	private UserRepository repo;
 
-	public List< User> findAll() {
+	public List<User> findAll() {
 		System.out.println(repo.findAll());
 		return repo.findAll();
 	}
@@ -24,5 +25,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
