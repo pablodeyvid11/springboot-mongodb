21c0b1fee43c2d76c5f23a6c1c516931631d9005
Pablo Deyvid <pablodeyvid11@gmail.com>
2020-03-29 22:10:22 -0300

    Obtendo um usuário por id

--- src/main/java/com/pablodeyvid/springbootmongodb/services/UserService.java
+++ src/main/java/com/pablodeyvid/springbootmongodb/services/UserService.java
@@ -1,21 +1,28 @@
 package com.pablodeyvid.springbootmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pablodeyvid.springbootmongodb.domain.User;
 import com.pablodeyvid.springbootmongodb.repository.UserRepository;
+import com.pablodeyvid.springbootmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll() {
+
+	public List< User> findAll() {
 		System.out.println(repo.findAll());
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
