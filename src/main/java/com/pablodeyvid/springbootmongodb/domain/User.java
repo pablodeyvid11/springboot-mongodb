33cef7ddd94cc2901befbe1ba7268da7a7572a82
Pablo Deyvid <pablodeyvid11@gmail.com>
2020-04-01 11:47:14 -0300

    Referenciando os posts do usuário

--- src/main/java/com/pablodeyvid/springbootmongodb/domain/User.java
+++ src/main/java/com/pablodeyvid/springbootmongodb/domain/User.java
@@ -1,19 +1,28 @@
 package com.pablodeyvid.springbootmongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection="user")
+@Document(collection = "user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private String name;
 	private String email;
-	
+
+	@DBRef(/*
+			 * Garante que os post só vão serem carregados, expicitamente, quando é chamada
+			 * a lista
+			 */lazy = true)
+	private List<Post> posts = new ArrayList<>();
+
 	public User() {
 	}
 
@@ -46,6 +55,14 @@ public class User implements Serializable {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
